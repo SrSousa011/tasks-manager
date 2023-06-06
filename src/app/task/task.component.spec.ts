f8779221fc9a7ea4334279c022f487e2b74f2dc9
Lucas Sousa <lucas.sousa_99@hotmail.com>
2023-06-06 12:23:44 -0300

    [Add] Angular Project

--- src/app/task/task.component.spec.ts
+++ src/app/task/task.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TaskComponent } from './task.component';
+import { TarefaComponent } from './tarefa.component';
 
-describe('TaskComponent', () => {
-  let component: TaskComponent;
-  let fixture: ComponentFixture<TaskComponent>;
+describe('TarefaComponent', () => {
+  let component: TarefaComponent;
+  let fixture: ComponentFixture<TarefaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TarefaComponent ]
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TaskComponent]
-    });
-    fixture = TestBed.createComponent(TaskComponent);
+    fixture = TestBed.createComponent(TarefaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -7,25 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit{
 
-  tasksList: any[] = [];
+  TAREFA_KEY = 'tarefa_key'
+  tasksList : any[] = []
+  constructor() { }
 
-  constructor(){}
+  ngOnInit(): void {
+     const tasks = localStorage.getItem(this.TAREFA_KEY)
+    if (tasks){
+      this.tasksList = JSON.parse(tasks)
+    }
+  }
 
-  
- ngOnInit(): void {
-    this.tasksList = [
-      { id: 1, name: 'Work out', completed: false },
-      { id: 2, name: 'Learn and explore new skills', completed: true },
-      { id: 3, name: 'Collaborate with team members', completed: true },
-      { id: 4, name: 'Check and respond to emails', completed: true },
-      { id: 5, name: 'Attend meetings and collaborate with colleagues', completed: true },
-      { id: 6, name: 'Work on assigned tasks or projects', completed: true },
-      { id: 7, name: 'Collaborate witth team member', completed: true },
-      { id: 8, name: 'Review and Update project status', completed: true },
-      { id: 9, name: 'Reflect and plan for the next da', completed: true },
-      { id: 10, name: 'Plan and prioritize tasks for the day', completed: true }
-    ];
-  } 
   add(addNewTask: string){
     if(addNewTask.trim().length == 0){
       return;
@@ -41,4 +33,23 @@ export class TaskComponent implements OnInit{
 delete(id: number) {
   this.tasksList = this.tasksList.filter(task => task.id !== id);
 }
+
+  complete(id: number) {
+    const taskFound = this.tasksList.find( item => item.id == id)
+
+    if (taskFound){
+      taskFound.completed = !taskFound.completed
+      this.saveList()
+    }
+
+  deleteSelected() {
+    this.tasksList = this.tasksList.filter((task) => !task.selected);
+    this.saveList();
+  }
+
+  }
+   private saveList(){
+    localStorage.setItem(this.TAREFA_KEY, JSON.stringify(this.tasksList))
+  }
+
 }
